1a04099fea05108a187b5d1c9f836bd66a5094d5
Ricardo Dantas <ricardodantas@gmail.com>
2014-07-25 02:11:11 -0300

    update tests

--- tests/casperjs/general.js
+++ tests/casperjs/general.js
@@ -1,70 +1,70 @@
-casper.test.begin('[HOME] Verificar classes gerais',function suite(test) {
-    casper.start("http://localhost:8080/templates/x-home.html", function() {
+casper.test.begin('[HOME] Verificar classes gerais', function suite(test) {
+  casper.start("http://localhost:8080/templates/x-home.html", function() {
 
 
-        test.assertExists('#x-header');
-        test.assertExists('#x-footer');
-        test.assertExists('.x-wrapper');
-        test.assertExists('.x-main');
-        test.assertExists('[id="header-tags"]','[<vtex:metaTags />] Vtex subtemplate included.');
+    test.assertExists('#x-header');
+    test.assertExists('#x-footer');
+    test.assertExists('.x-wrapper');
+    test.assertExists('.x-main');
+    test.assertExists('[id="header-tags"]', '[<vtex:metaTags />] Vtex subtemplate included.');
 
-    });
+  });
 
-    casper.run(function() {
-        test.done();
-    });
+  casper.run(function() {
+    test.done();
+  });
 });
 
 
-casper.test.begin('[PRODUCT LIST] Verificar classes gerais',function suite(test) {
-    casper.start("http://localhost:8080/templates/x-product-list.html", function() {
+casper.test.begin('[PRODUCT LIST] Verificar classes gerais', function suite(test) {
+  casper.start("http://localhost:8080/templates/x-product-list.html", function() {
 
-        test.assertExists('.x-breadcrumb');
-        test.assertExists('#x-header');
-        test.assertExists('#x-footer');
-        test.assertExists('.x-wrapper');
-        test.assertExists('.x-main');
-        test.assertExists('[id="header-tags"]','[<vtex:metaTags />] Vtex subtemplate included.');
-    });
+    test.assertExists('.x-breadcrumb');
+    test.assertExists('#x-header');
+    test.assertExists('#x-footer');
+    test.assertExists('.x-wrapper');
+    test.assertExists('.x-main');
+    test.assertExists('[id="header-tags"]', '[<vtex:metaTags />] Vtex subtemplate included.');
+  });
 
-    casper.run(function() {
-        test.done();
-    });
+  casper.run(function() {
+    test.done();
+  });
 });
 
 
-casper.test.begin('[PRODUCT] Verificar classes gerais',function suite(test) {
-    casper.start("http://localhost:8080/templates/x-product.html", function() {
+casper.test.begin('[PRODUCT] Verificar classes gerais', function suite(test) {
+  casper.start("http://localhost:8080/templates/x-product.html", function() {
 
-        test.assertExists('.x-breadcrumb');
-        test.assertExists('#x-header');
-        test.assertExists('#x-footer');
-        test.assertExists('.x-wrapper');
-        test.assertExists('.x-main');
-        test.assertExists('[id="header-tags"]','[<vtex:metaTags />] Vtex subtemplate included.');
+    test.assertExists('.x-breadcrumb');
+    test.assertExists('#x-header');
+    test.assertExists('#x-footer');
+    test.assertExists('.x-wrapper');
+    test.assertExists('.x-main');
+    test.assertExists('[id="header-tags"]', '[<vtex:metaTags />] Vtex subtemplate included.');
 
-    });
+  });
 
-    casper.run(function() {
-        test.done();
-    });
+  casper.run(function() {
+    test.done();
+  });
 });
 
 
 casper.test.begin('[STANDARD] Verificar classes gerais', function suite(test) {
-    casper.start("http://localhost:8080/templates/x-standard.html", function() {
+  casper.start("http://localhost:8080/templates/x-standard.html", function() {
 
-        test.assertExists('.x-breadcrumb');
+    test.assertExists('.x-breadcrumb');
 
-        test.assertExists('#x-header');
-        test.assertExists('#x-footer');
-        test.assertExists('.x-wrapper');
-        test.assertExists('.x-main');
-        test.assertExists('[id="header-tags"]','[<vtex:metaTags />] Vtex subtemplate included.');
+    test.assertExists('#x-header');
+    test.assertExists('#x-footer');
+    test.assertExists('.x-wrapper');
+    test.assertExists('.x-main');
+    test.assertExists('[id="header-tags"]', '[<vtex:metaTags />] Vtex subtemplate included.');
 
-    });
+  });
 
-    casper.run(function() {
-        test.done();
-    });
+  casper.run(function() {
+    test.done();
+  });
 });
