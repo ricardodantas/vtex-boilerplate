7e1879d37718226e7f87324c703ecb0953d1f553
Ricardo Dantas <ricardodantas@gmail.com>
2014-07-26 01:11:56 -0300

    update classes and added csscss and csslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,183 +1,207 @@
 module.exports = function(grunt) {
-    var config, environment, errorHandler, name, open, pkg, taskArray, taskName, tasks, verbose, _results;
-    pkg = grunt.file.readJSON('package.json');
-    environment = process.env.VTEX_HOST || 'vtexcommerce';
-    verbose = grunt.option('verbose');
-    open = pkg.accountName ? "http://" + pkg.accountName + ".vtexlocal.com.br/?debugcss=true&debugjs=true" : void 0;
-    errorHandler = function(err, req, res, next) {
-        var errString, _ref, _ref1;
-        errString = (_ref = (_ref1 = err.code) != null ? _ref1.red : void 0) != null ? _ref : err.toString().red;
-        return grunt.log.warn(errString, req.url.yellow);
-    };
-    config = {
-        clean: {
-            main: ['build']
+  var config, environment, errorHandler, name, open, pkg, taskArray, taskName, tasks, verbose, _results;
+  pkg = grunt.file.readJSON('package.json');
+  environment = process.env.VTEX_HOST || 'vtexcommerce';
+  verbose = grunt.option('verbose');
+  open = pkg.accountName ? "http://" + pkg.accountName + ".vtexlocal.com.br/?debugcss=true&debugjs=true" : void 0;
+  errorHandler = function(err, req, res, next) {
+    var errString, _ref, _ref1;
+    errString = (_ref = (_ref1 = err.code) != null ? _ref1.red : void 0) != null ? _ref : err.toString().red;
+    return grunt.log.warn(errString, req.url.yellow);
+  };
+  config = {
+    clean: {
+      main: ['build']
+    },
+    coffee: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src/assets/coffeescript/',
+          src: ['**/*.coffee'],
+          dest: "build/assets/js/",
+          ext: '.js'
+        }]
+      }
+    },
+    coffeelint: {
+      options: {
+        'coffeescript_error': {
+          'level': 'error'
         },
-        coffee: {
-            main: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/assets/coffeescript/',
-                    src: ['**/*.coffee'],
-                    dest: "build/assets/js/",
-                    ext: '.js'
-                }]
-            }
+        'no_trailing_whitespace': {
+          'level': 'error'
         },
-        coffeelint: {
-            options: {
-                'coffeescript_error': {
-                    'level': 'error'
-                },
-                'no_trailing_whitespace': {
-                    'level': 'error'
-                },
-                'camel_case_classes': {
-                    'level': 'error'
-                }
-            },
-            app: ['src/assets/coffeescript/*.coffee']
-        },
-        compass: {
-            dist: {
-                options: {
-                    sassDir: 'src/assets/scss/',
-                    cssDir: 'build/assets/css/',
-                    imagesDir: 'src/assets/img/',
-                    environment: 'production'
-                }
-            }
-        },
-        uglify: {
-            options: {
-                mangle: false
-            },
-            main: {
-                files: [{
-                    expand: true,
-                    cwd: 'build/assets/js/',
-                    src: ['*.js', ],
-                    dest: 'build/assets/js/',
-                    ext: '.js'
-                }]
-            }
-        },
-        imagemin: {
-            main: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/assets/img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'build/assets/img'
-                }]
-            }
-        },
-        sprite: {
-            all: {
-                src: 'src/assets/img/sprite/*.png',
-                destImg: 'build/assets/img/x-sprite.png',
-                destCSS: 'src/assets/scss/_x-sprite.scss',
-                cssFormat: 'scss'
-            }
-        },
-        casperjs: {
-            options: {
-                async: {
-                    parallel: false
-                }
-            },
-            files: ['tests/casperjs/**/*.js']
-        },
-        connect: {
-            server_test: {
-                options: {
-                    // useAvailablePort: true,
-                    hostname: 'localhost',
-                    port: 8080,
-                    base: 'src/',
-                    open: false
-                }
-            },
-            http: {
-                options: {
-                    hostname: "*",
-                    open: open,
-                    port: process.env.PORT || 8080,
-                    middleware: [
-                        require('connect-livereload')({
-                            disableCompression: true
-                        }), require('connect-http-please')({
-                            replaceHost: (function(h) {
-                                return h.replace("vtexlocal", environment);
-                            })
-                        }, {
-                            verbose: verbose
-                        }), require('connect-tryfiles')('**', "http://portal." + environment + ".com.br:80", {
-                            cwd: 'build/',
-                            verbose: verbose
-                        }), require('connect')["static"]('./build/'), errorHandler
-                    ]
-                }
-            }
-        },
-        styledocco: {
-            dist: {
-                options: {
-                    name: 'Vtex Boilerplate'
-                },
-                files: {
-                    'docs': 'src/assets/scss'
-                }
-            }
-        },
-        watch: {
-            options: {
-                livereload: 1337
-            },
-            coffee: {
-                files: ['src/assets/coffeescript/**/*.coffee'],
-                tasks: ['coffee', 'coffeelint']
-            },
-            compass: {
-                options: {
-                    livereload: false
-                },
-                files: ['src/assets/scss/**/*.scss'],
-                tasks: ['compass', 'styledocco']
-            },
-            images: {
-                files: ['src/assetsimg/**/*.{png,jpg,gif}'],
-                tasks: ['sprite', 'imagemin']
-            },
-            css: {
-                files: ['build/css/**/*.css']
-            }
-            // main: {
-            //     files: ['src/templates/**/*.html', 'src/**/*.js', 'src/**/*.css'],
-            //     tasks: ['copy']
-            // }
-
+        'camel_case_classes': {
+          'level': 'error'
         }
-    };
-
-    tasks = {
-        build: ['clean', 'sprite', 'compass', 'coffee', 'coffeelint', 'imagemin', 'uglify', 'styledocco'],
-        test: ['connect:server_test', 'casperjs'],
-        server_test: ['connect:server_test:keepalive'],
-        doc: ['styledocco'],
-        "default": ['build', 'connect', 'watch'],
-        devmin: ['build', 'min', 'connect:http:keepalive']
-    };
-    grunt.initConfig(config);
-    for (name in pkg.devDependencies) {
-        if (name.slice(0, 6) === 'grunt-') {
-            grunt.loadNpmTasks(name);
+      },
+      app: ['src/assets/coffeescript/*.coffee']
+    },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'src/assets/scss/',
+          cssDir: 'build/assets/css/',
+          imagesDir: 'src/assets/img/',
+          environment: 'production'
         }
+      }
+    },
+    csscss: {
+      options: {
+        colorize: true,
+        verbose: true,
+        outputJson: false,
+        minMatch: 5,
+        compass: true
+      },
+      dist: {
+        src: ['src/assets/scss/**/*.scss']
+      }
+    },
+    csslint: {
+      strict: {
+        options: {
+          import: false,
+          important: true,
+          'adjoining-classes': false,
+          ids: false,
+          'zero-units': false
+        },
+        src: ['build/assets/css/**/*.css']
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'build/assets/js/',
+          src: ['*.js', ],
+          dest: 'build/assets/js/',
+          ext: '.js'
+        }]
+      }
+    },
+    imagemin: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src/assets/img/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'build/assets/img'
+        }]
+      }
+    },
+    sprite: {
+      all: {
+        src: 'src/assets/img/sprite/*.png',
+        destImg: 'build/assets/img/x-sprite.png',
+        destCSS: 'src/assets/scss/_x-sprite.scss',
+        cssFormat: 'scss'
+      }
+    },
+    casperjs: {
+      options: {
+        async: {
+          parallel: false
+        }
+      },
+      files: ['tests/casperjs/**/*.js']
+    },
+    connect: {
+      server_test: {
+        options: {
+          // useAvailablePort: true,
+          hostname: 'localhost',
+          port: 8080,
+          base: 'src/',
+          open: false
+        }
+      },
+      http: {
+        options: {
+          hostname: "*",
+          open: open,
+          port: process.env.PORT || 8080,
+          middleware: [
+            require('connect-livereload')({
+              disableCompression: true
+            }), require('connect-http-please')({
+              replaceHost: (function(h) {
+                return h.replace("vtexlocal", environment);
+              })
+            }, {
+              verbose: verbose
+            }), require('connect-tryfiles')('**', "http://portal." + environment + ".com.br:80", {
+              cwd: 'build/',
+              verbose: verbose
+            }), require('connect')["static"]('./build/'), errorHandler
+          ]
+        }
+      }
+    },
+    styledocco: {
+      dist: {
+        options: {
+          name: 'Vtex Boilerplate'
+        },
+        files: {
+          'docs': 'src/assets/scss'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: 1337
+      },
+      coffee: {
+        files: ['src/assets/coffeescript/**/*.coffee'],
+        tasks: ['coffee', 'coffeelint']
+      },
+      compass: {
+        options: {
+          livereload: false
+        },
+        files: ['src/assets/scss/**/*.scss'],
+        tasks: ['compass', 'styledocco']
+      },
+      images: {
+        files: ['src/assetsimg/**/*.{png,jpg,gif}'],
+        tasks: ['sprite', 'imagemin']
+      },
+      css: {
+        files: ['build/css/**/*.css']
+      }
+      // main: {
+      //     files: ['src/templates/**/*.html', 'src/**/*.js', 'src/**/*.css'],
+      //     tasks: ['copy']
+      // }
+
     }
-    _results = [];
-    for (taskName in tasks) {
-        taskArray = tasks[taskName];
-        _results.push(grunt.registerTask(taskName, taskArray));
+  };
+
+  tasks = {
+    build: ['clean', 'sprite', 'compass', 'csscss', 'csslint', 'coffee', 'coffeelint', 'imagemin', 'uglify', 'styledocco'],
+    test: ['connect:server_test', 'casperjs'],
+    server_test: ['connect:server_test:keepalive'],
+    doc: ['styledocco'],
+    "default": ['build', 'connect', 'watch'],
+    devmin: ['build', 'min', 'connect:http:keepalive']
+  };
+  grunt.initConfig(config);
+  for (name in pkg.devDependencies) {
+    if (name.slice(0, 6) === 'grunt-') {
+      grunt.loadNpmTasks(name);
     }
-    return _results;
+  }
+  _results = [];
+  for (taskName in tasks) {
+    taskArray = tasks[taskName];
+    _results.push(grunt.registerTask(taskName, taskArray));
+  }
+  return _results;
 };
